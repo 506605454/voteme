45a483f488db93360f16a1de2cf5c58b2bf1f13f
DavidHuang <506605454@qq.com>
2016-11-27 16:22:34 +0800

    添加表

---
+++ src/main/java/com/hqw/voteme/service/vote/VoteService.java
@@ -0,0 +1,10 @@
+package com.hqw.voteme.service.vote;
+
+/**
+ * Created by Administrator on 2016/11/27.
+ */
+public interface VoteService {
+
+
+
+}
---
+++ src/main/java/com/hqw/voteme/service/vote/impl/VoteServiceImpl.java
@@ -0,0 +1,7 @@
+package com.hqw.voteme.service.vote.impl;
+
+/**
+ * Created by Administrator on 2016/11/27.
+ */
+public class VoteServiceImpl {
+}
